8af2e82b2aef55d3f4961e794f8406bf6149d327
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-07 15:46:54 -0500

    Creando clase recursiva

---
+++ src/com/potoware/recursividad/ejemplo/EjemploRecursividad.java
@@ -0,0 +1,37 @@
+package com.potoware.recursividad.ejemplo;
+
+import com.potoware.recursividad.ejemplo.models.Componente;
+
+public class EjemploRecursividad {
+    public static void main(String[] args) {
+        Componente pc = new Componente("Gabinente PC Dell");
+        Componente poder = new Componente("Fuente Poder 700W");
+        Componente placaMadre = new Componente("MainBoard Dell sockets intel ");
+        Componente cpu = new Componente("intel i7 8000X");
+        Componente ventilador = new Componente("Ventilador 2800RPM");
+        Componente discipador = new Componente("Discipador metalico");
+        Componente tv = new Componente("Titan RTX 3000 30GB");
+        Componente gpu = new Componente("Titan nvidia RTX");
+        Componente gpuRam = new Componente("15GB");
+        Componente gpuRam2 = new Componente("15GB");
+        Componente gpuVentiladores = new Componente("Ventiladores");
+        Componente ram = new Componente("Memoria RAM 32GB");
+        Componente ssd = new Componente("Disco SSD 2TB");
+
+        cpu.addComponente(ventilador).addComponente(discipador);
+        tv.addComponente(gpu)
+                .addComponente(gpuRam)
+                .addComponente(gpuRam2)
+                .addComponente(gpuVentiladores);
+
+        placaMadre.addComponente(cpu)
+                .addComponente(tv)
+                .addComponente(ram)
+                .addComponente(ssd);
+
+        pc.addComponente(poder)
+                .addComponente(placaMadre)
+                .addComponente(new Componente("Teclado"))
+                .addComponente(new Componente("Mouse"));
+    }
+}
